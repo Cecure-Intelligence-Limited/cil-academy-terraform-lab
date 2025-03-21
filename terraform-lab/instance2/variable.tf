4be524f63a59f5b8f19ad9ec9889a3f5b4c8ac8d
b3c3 <b3c3@cecureintel.com>
2025-03-21 10:31:32 +0000

    Corrected subnet CIDR block in variable.tf

--- terraform-lab/instance2/variable.tf
+++ terraform-lab/instance2/variable.tf
@@ -11,7 +11,7 @@ variable "vpc_cidr_block" {
 variable "subnet_cidr_block" {
   description = "The CIDR block for the subnet"
   type        = string
-  default     = "10.0.1.0/27"
+  default     = "10.0.0.0/27"
 }
 
 variable "availability_zone" {
